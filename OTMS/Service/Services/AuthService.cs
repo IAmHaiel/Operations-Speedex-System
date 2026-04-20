3cbf16e3a2ff371768bfb1b600244498eabaeb29
IAmHaiel <mikhjnelo@gmail.com>
2026-04-20 19:11:34 +0800

    Added LoginAsync functionalities

--- OTMS/Service/Services/AuthService.cs
+++ OTMS/Service/Services/AuthService.cs
@@ -1,15 +1,34 @@
-﻿using OTMS.Data;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using OTMS.Data;
 using OTMS.Entities.DTOs;
 using OTMS.Entities.Models;
 using OTMS.Service.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace OTMS.Service.Services
 {
     public class AuthService(OTMSDbContext context, IConfiguration configuration) : IAuthService
     {
-        public Task<TokenResponseDTO?> LoginAsync(EmployeeLoginDTO request)
+
+        public async Task<TokenResponseDTO?> LoginAsync(EmployeeLoginDTO request)
         {
-            throw new NotImplementedException();
+            var employee = await context.Employees.FirstOrDefaultAsync(u => u.EmployeeNumber == request.EmployeeNumber);
+
+            if (employee is null)
+            {
+                return null;
+            }
+            if (new PasswordHasher<Employee>().VerifyHashedPassword(employee, employee.PasswordHash, request.Password) == PasswordVerificationResult.Failed)
+            {
+                return null;
+            }
+
+            return await CreateTokenResponse(employee);
         }
 
         public Task<TokenResponseDTO?> RefreshTokensAsync(RefreshTokenRequestDTO request)
@@ -21,5 +40,62 @@ namespace OTMS.Service.Services
         {
             throw new NotImplementedException();
         }
+
+
+        // Helper Methods
+        private async Task<TokenResponseDTO> CreateTokenResponse(Employee employee)
+        {
+            return new TokenResponseDTO
+            {
+                AccessToken = CreateToken(employee),
+                RefreshToken = await GenerateAndSaveRefreshTokenAsync(employee)
+            };
+        }
+
+        private async Task<string> GenerateAndSaveRefreshTokenAsync(Employee employee)
+        {
+            var refreshToken = GenerateRefreshToken();
+            employee.RefreshToken = refreshToken;
+            employee.RefreshTokenExpiryTime = DateTime.UtcNow.AddDays(7);
+            await context.SaveChangesAsync();
+            return refreshToken;
+        }
+
+        private string GenerateRefreshToken()
+        {
+            var randomNumber = new byte[32];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
+        }
+
+        private string CreateToken(Employee employee)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, employee.EmployeeNumber),
+                new Claim(ClaimTypes.NameIdentifier, employee.Id.ToString()),
+                new Claim(ClaimTypes.Role, employee.Role)
+            };
+
+            //Signing Key
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(configuration.GetValue<string>("AppSettings:Token")!));
+
+            // Signing Credentials
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            // Descriptor that describes the JSON WEB TOKEN
+            var tokenDescriptor = new JwtSecurityToken(
+                issuer: configuration.GetValue<string>("AppSettings:Issuer"),
+                audience: configuration.GetValue<string>("AppSettings:Audience"),
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials: creds
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+        }
+
     }
 }
