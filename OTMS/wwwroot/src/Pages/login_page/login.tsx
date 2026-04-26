267325cc013a06139dcf10a541f2c3e01e3eb115
IAmHaiel <mikhjnelo@gmail.com>
2026-04-26 13:45:23 +0800

    Corrected fetch response URI in login.tsx

--- OTMS/wwwroot/src/Pages/login_page/login.tsx
+++ OTMS/wwwroot/src/Pages/login_page/login.tsx
@@ -16,7 +16,7 @@ export default function Login() {
         setStatusType('');
 
         try {
-            const response = await fetch('/api/Auth/login', {
+            const response = await fetch('/api/authentication/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
