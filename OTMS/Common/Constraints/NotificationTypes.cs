3441fefc212ed203ced6867b083c6d87b5a5dded
IAmHaiel <mikhjnelo@gmail.com>
2026-05-24 17:05:45 +0800

    Added a Notification Types Constraints

---
+++ OTMS/Common/Constraints/NotificationTypes.cs
@@ -0,0 +1,8 @@
+﻿namespace OTMS.Common.Constraints
+{
+    public class NotificationTypes
+    {
+        public const string TaskAssigned = "TaskAssigned";
+        public const string TaskDeadlineApproaching = "Task Deadline Approaching";
+    }
+}
