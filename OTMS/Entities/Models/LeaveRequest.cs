9d70decde648f9551402f94e61dffd7dbe27156e
IAmHaiel <mikhjnelo@gmail.com>
2026-05-24 11:00:01 +0800

    Added LeaveRequest Model and added Navigation Properties in Account

---
+++ OTMS/Entities/Models/LeaveRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace OTMS.Entities.Models
+{
+    public class LeaveRequest
+    {
+        public Guid LeaveId { get; set; }
+        public Guid? Approved_By { get; set; }
+
+        public DateTime Start_Date { get; set; }
+        public DateTime End_Date { get; set; }
+        public string Reason { get; set; } = string.Empty;
+        public string Approval_Status { get; set; } = "Pending";
+
+        // Navigation properties
+        public Account Account { get; set; } // Submitter
+        public Account ApprovedByAccount { get; set; } // Approver
+    }
+}
