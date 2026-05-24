3a02df0c94ec0f20db658f8f7dc85de0702bbcb7
IAmHaiel <mikhjnelo@gmail.com>
2026-05-24 17:28:40 +0800

    Updated INotificationService

--- OTMS/Service/Interfaces/INotificationService.cs
+++ OTMS/Service/Interfaces/INotificationService.cs
@@ -1,4 +1,5 @@
-﻿using OTMS.Entities.Models;
+﻿using OTMS.Entities.DTOs.Notification.Responses;
+using OTMS.Entities.Models;
 
 namespace OTMS.Service.Interfaces
 {
@@ -6,5 +7,8 @@ namespace OTMS.Service.Interfaces
     {
         System.Threading.Tasks.Task CreateTaskAssignedNotificationAsync(Entities.Models.Task task);
         System.Threading.Tasks.Task CreateDeadlineNotificationAsync(Entities.Models.Task task);
+        System.Threading.Tasks.Task<List<NotificationResponseDTO>> GetMyNotificationsAsync();
+        System.Threading.Tasks.Task<bool> MarkNotificationAsReadAsync(Guid notificationId);
+        System.Threading.Tasks.Task CheckTaskDeadlinesAsync();
     }
 }
